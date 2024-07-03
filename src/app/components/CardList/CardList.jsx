4e8271d11469dadbbd8b2a69ac613c9b5f3ebda4
Soumyabrata Samanta <129324238+soumyabrata-samanta@users.noreply.github.com>
2024-07-03 09:15:19 +0530

    Update CardList.jsx

--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -5,7 +5,7 @@ import Card from '../card/Card';
 import Image from 'next/image';
 
 const getData = async (page,cat)=>{
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,{
+  const res = await fetch(`https://mind-canvas-git-main-soumyabrata-samantas-projects.vercel.app/api/posts?page=${page}&cat=${cat || ""}`,{
       cache:"no-store",
   });
   if(!res.ok){
