0b8693c2c222136a5f2a14b48da417d2cb840f7a
Soumyabrata Samanta <129324238+soumyabrata-samanta@users.noreply.github.com>
2024-07-03 09:38:11 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,11 @@ const page = () => {
             <Image src="/profile.jpg" alt="" fill className={Styles.avatar} />
             </div>
       <p className={Styles.postDesc}>
- Hello, I am Soumyabrata Samanta, a passionate third Year Computer Science Engineer currently pursuing my studies at Kalinga Institute of Industrial Technology in Bhubaneswar.<br /> <br />With a keen interest in technology and a drive for innovation, I am deeply passionate about developing interactive full-stack websites that not only function seamlessly but also engage users in meaningful ways.<br /><br/> I believe in the power of technology to transform ideas into reality, and I am committed to honing my skills to contribute positively to the digital landscape.< br /> <br /> 
+ Hello, I am Soumyabrata Samanta, a passionate third Year Computer Science Engineer currently pursuing my studies at Kalinga Institute of Industrial Technology in Bhubaneswar.
+        <br /> <br />
+        With a keen interest in technology and a drive for innovation, I am deeply passionate about developing interactive full-stack websites that not only function seamlessly but also engage users in meaningful ways.
+        <br /><br/> I believe in the power of technology to transform ideas into reality, and I am committed to honing my skills to contribute positively to the digital landscape.
+        < br /> <br /> 
  <div className={Styles.details}>
  Feel free to contact me here: <br /> <br />
    <p >Email:<span className={Styles.in}> ankansam@gmail.com</span> </p> <br />
